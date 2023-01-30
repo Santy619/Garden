4600c862cbebf53a57fa364515d6bb8131df213f
Santy619 <102596209+Santy619@users.noreply.github.com>
2023-01-30 20:03:18 -0300

    V7
    version 1.7 del proyecto semestral Garden

--- Garden/core/static/core/js/validaciones.js
+++
@@ -1,7 +0,0 @@
-function validarFormulario() {
-    let x = document.forms["myForm"]["fname"].value;
-    if (x == "") {
-      alert("El campo debe ser rellenado");
-      return false;
-    }
-  }
--- Garden/rest_planta/serializers.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import fields
-from rest_framework import serializers
-from core.models import *
-
-class PlantaSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Planta
-        fields =[ 'idPlanta' , 'nombre', 'precio', 'stock', 'idCategoria' , 'imagen' ]
